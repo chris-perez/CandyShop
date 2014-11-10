7655d5ec62db2fb158e5c42f5580eb3e0354afe9
noahzheutlin <noahz@rcn.com>
2014-11-10 15:24:41 -0500

    new
    new

---
+++ CandyShop/Candy.h
@@ -0,0 +1,14 @@
+//
+//  Candy.h
+//  CandyShopProject
+//
+//  Created by Noah on 11/10/14.
+//  Copyright (c) 2014 Noah. All rights reserved.
+//
+
+#ifndef __CandyShopProject__Candy__
+#define __CandyShopProject__Candy__
+
+#include <stdio.h>
+
+#endif /* defined(__CandyShopProject__Candy__) */
--- CandyShop/main.cpp
+++ CandyShop/main.cpp
@@ -1,13 +1,9 @@
 //
-//  CandyShop.h
+//  main.cpp
 //  CandyShopProject
 //
 //  Created by Noah on 11/10/14.
 //  Copyright (c) 2014 Noah. All rights reserved.
 //
 
-#ifndef CandyShopProject_CandyShop_h
-#define CandyShopProject_CandyShop_h
-
-
-#endif
+#include <stdio.h>
